a07c305afd31b1b819ad9656896bbcebda346b1c
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-05-23 14:44:28 +0300

    +push

--- homework-24-05/main.js
+++ homework-24-05/main.js
@@ -1,30 +1,34 @@
 let c;
-let str = 0;
-let number = 0;
+// let str = 0;
+// let number = 0;
+let str = [];
+let number = [];
 
 let array = [
-  7,
-  "привет",
-  10 < 15,
-  null,
-  c,
-  { color: "red" },
-  [10, 20, 30],
-  26.98
+    7,
+    "привет",
+    10 < 15,
+    null,
+    c,
+    { color: "red" },
+    [10, 20, 30],
+    26.98
 ];
 
 for (let i = 0; i < array.length; i++) {
-  if (typeof array[i] === "number") {
-    number++;
-  } else if (typeof array[i] === "string") {
-    str++;
-  } else if (Array.isArray(array[i])) {
-    for (let j = 0; j < array[i].length; j++) {
-      if (typeof array[i][j] === "number") {
-        number++;
-      }
+    if (typeof array[i] === "number") {
+        number.push(array[i]);
+    } else if (typeof array[i] === "string") {
+        str.push(array[i]);
+    } else if (Array.isArray(array[i])) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (typeof array[i][j] === "number") {
+                number.push(array[i][j]);
+            } else if (typeof array[i][j] === "string") {
+                str.push(array[i][j]);
+            }
+        }
     }
-  }
 }
 
 console.log(`str: ${str}\nnumber: ${number}`);
