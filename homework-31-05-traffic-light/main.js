704acec56ab58c41d36d5a090894b710b5785253
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-05-31 00:06:50 +0300

    push changes at the cycle

--- homework-31-05-traffic-light/main.js
+++ homework-31-05-traffic-light/main.js
@@ -2,16 +2,21 @@ let green_lights = document.querySelectorAll('.green_light');
 let red_lights = document.querySelectorAll('.red_light');
 const green_color = "#00FF00";
 const red_color = "#DC143C";
+console.log(red_lights);
 
 for (let i = 0; i < red_lights.length; i++) {
 
     red_lights[i].addEventListener('mouseover', function() {
-        red_lights[i].style.backgroundColor = green_color;
-        green_lights[i].style.backgroundColor = red_color;
+        for (let i = 0; i < red_lights.length; i++) {
+            red_lights[i].style.backgroundColor = green_color;
+            green_lights[i].style.backgroundColor = red_color;
+        }
     });
 
     red_lights[i].addEventListener('mouseout', function() {
-        red_lights[i].style.backgroundColor = red_color;
-        green_lights[i].style.backgroundColor = green_color;
+        for (let i = 0; i < red_lights.length; i++) {
+            red_lights[i].style.backgroundColor = red_color;
+            green_lights[i].style.backgroundColor = green_color;
+        }
     });
 }
