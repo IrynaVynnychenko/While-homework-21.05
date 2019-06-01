11cbe0cd7aa28c2e8498c3856264f25ec2bd91af
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-06-01 19:01:08 +0300

    push traffic-light with button

--- homework-31-05-traffic-light/main.js
+++ homework-31-05-traffic-light/main.js
@@ -1,42 +1,55 @@
+// for (let i = 0; i < red_lights.length; i++) {
+
+//     red_lights[i].addEventListener('mouseover', function() {
+//         for (let i = 0; i < red_lights.length; i++) {
+//             red_lights[i].style.backgroundColor = green_color;
+//             green_lights[i].style.backgroundColor = red_color;
+//         }
+//     });
+
+//     red_lights[i].addEventListener('mouseout', function() {
+//         for (let i = 0; i < red_lights.length; i++) {
+//             red_lights[i].style.backgroundColor = red_color;
+//             green_lights[i].style.backgroundColor = green_color;
+//         }
+//     });
+
+// }
+
+
+// for (let i = 0; i < green_lights.length; i++) {
+
+//     green_lights[i].addEventListener('mouseover', function() {
+//         for (let i = 0; i < green_lights.length; i++) {
+//             red_lights[i].style.backgroundColor = green_color;
+//             green_lights[i].style.backgroundColor = red_color;
+//         }
+//     });
+
+//     green_lights[i].addEventListener('mouseout', function() {
+//         for (let i = 0; i < green_lights.length; i++) {
+//             red_lights[i].style.backgroundColor = red_color;
+//             green_lights[i].style.backgroundColor = green_color;
+//         }
+//     });
+// }
+
 let green_lights = document.querySelectorAll('.green_light');
 let red_lights = document.querySelectorAll('.red_light');
-const green_color = "#00FF00";
-const red_color = "#DC143C";
-console.log(red_lights);
+const green_color = "rgb(0, 255, 0)";
+const red_color = "rgb(220, 20, 60)";
+let change = document.querySelector('button');
 
-for (let i = 0; i < red_lights.length; i++) {
+change.addEventListener('click', function() {
 
-    red_lights[i].addEventListener('mouseover', function() {
-        for (let i = 0; i < red_lights.length; i++) {
-            red_lights[i].style.backgroundColor = green_color;
-            green_lights[i].style.backgroundColor = red_color;
+    for (let j = 0; j < green_lights.length; j++) {
+        if (window.getComputedStyle(green_lights[j]).getPropertyValue("background-color") == green_color) {
+            red_lights[j].style.backgroundColor = green_color;
+            green_lights[j].style.backgroundColor = red_color;
+        } else {
+            red_lights[j].style.backgroundColor = red_color;
+            green_lights[j].style.backgroundColor = green_color;
         }
-    });
-
-    red_lights[i].addEventListener('mouseout', function() {
-        for (let i = 0; i < red_lights.length; i++) {
-            red_lights[i].style.backgroundColor = red_color;
-            green_lights[i].style.backgroundColor = green_color;
-        }
-    });
-
-}
-
-
-for (let i = 0; i < green_lights.length; i++) {
-
-    green_lights[i].addEventListener('mouseover', function() {
-        for (let i = 0; i < green_lights.length; i++) {
-            red_lights[i].style.backgroundColor = green_color;
-            green_lights[i].style.backgroundColor = red_color;
-        }
-    });
-
-    green_lights[i].addEventListener('mouseout', function() {
-        for (let i = 0; i < green_lights.length; i++) {
-            red_lights[i].style.backgroundColor = red_color;
-            green_lights[i].style.backgroundColor = green_color;
-        }
-    });
-
-}
+        console.log(green_lights[j]);
+    }
+});
