5baf769bc82fc19531021a1ec312cb4f295c8fed
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-06-06 21:40:15 +0300

    push changes to button

--- homework-07-06-points/main.js
+++ homework-07-06-points/main.js
@@ -4,13 +4,9 @@ let tochka = document.querySelectorAll(".tochka");
 for (let i = 0; i < tochka.length; i++) {
     kvadrat[i].addEventListener('mousemove', function(e) {
         for (let j = 0; j < tochka.length; j++) {
-            if (j < tochka.length / 2) {
-                tochka[j].style.left = e.layerX + 'px';
-                tochka[j].style.top = e.offsetY + 'px';
-            } else {
-                tochka[j].style.right = e.layerX + 'px';
-                tochka[j].style.bottom = e.offsetY + 'px';
-            }
+            if (i == j) { tochka[j].style.display = 'none' } else { tochka[j].style.display = 'block' }
+            tochka[j].style.left = e.layerX + 'px';
+            tochka[j].style.top = e.offsetY + 'px';
         }
-    })
+    });
 };
