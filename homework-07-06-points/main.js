d7ec7f2b300fd81f0708117e94ed5484dfbb87c4
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-06-09 22:45:49 +0300

    push new homework and changes

--- homework-07-06-points/main.js
+++ homework-07-06-points/main.js
@@ -1,12 +1,21 @@
 let kvadrat = document.querySelectorAll(".kvadrat");
 let tochka = document.querySelectorAll(".tochka");
 
+
 for (let i = 0; i < tochka.length; i++) {
     kvadrat[i].addEventListener('mousemove', function(e) {
         for (let j = 0; j < tochka.length; j++) {
             if (i == j) { tochka[j].style.display = 'none' } else { tochka[j].style.display = 'block' }
-            tochka[j].style.left = e.layerX + 'px';
-            tochka[j].style.top = e.offsetY + 'px';
+            if (e.layerX < 4) {
+                tochka[j].style.left = '4px';
+            } else if (e.layerX < (kvadrat[i].offsetWidth - 4)) {
+                tochka[j].style.left = e.layerX + 'px';
+            }
+            if (e.layerY < 4) {
+                tochka[j].style.top = '4px';
+            } else if (e.layerY < (kvadrat[i].offsetHeight - 4)) {
+                tochka[j].style.top = e.layerY + 'px';
+            }
         }
     });
 };
